618fd93592a5e4b4891d0d8449ca3aaa3832f1be
ethroz <ethroz@gmail.com>
2025-01-15 15:50:05 -0400

    Started creating some unit tests for VirtualCube

--- Assets/Tests/VirtualCubeTest.cs
+++ Assets/Tests/VirtualCubeTest.cs
@@ -3,22 +3,35 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class VirtualCubeTest
-{
-    // A Test behaves as an ordinary method
+public class VirtualCubeTest {
     [Test]
-    public void VirtualCubeTestSimplePasses()
-    {
-        // Use the Assert class to test conditions
+    public void VirtualCubeGetStringState() {
+        VirtualCube cube = new();
+        var expected =
+            "YYY BBB RRR GGG OOO WWW\n" +
+            "YYY BBB RRR GGG OOO WWW\n" +
+            "YYY BBB RRR GGG OOO WWW\n";
+        Assert.AreEqual(expected, cube.GetStateString());
+    }
+
+    [Test]
+    public void VirtualCubeTestTopLayer() {
+        VirtualCube cube = new();
+        cube.TopLayer(true);
+        var expected =
+            "YYY RRR GGG OOO BBB WWW\n" +
+            "YYY BBB RRR GGG OOO WWW\n" +
+            "YYY BBB RRR GGG OOO WWW\n";
+        Assert.AreEqual(expected, cube.GetStateString());
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator VirtualCubeTestWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
+    // [UnityTest]
+    // public IEnumerator VirtualCubeTestWithEnumeratorPasses()
+    // {
+    //     // Use the Assert class to test conditions.
+    //     // Use yield to skip a frame.
+    //     yield return null;
+    // }
 }
